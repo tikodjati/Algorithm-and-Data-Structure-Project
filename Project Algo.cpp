47f3c4d1b791d4c75bab53e2a519d7a4927f0da5
oktfiaa <okta77131@gmail.com>
2025-05-01 18:02:46 +0700

    buat menu awal

--- Project Algo.cpp
+++ Project Algo.cpp
@@ -4,9 +4,22 @@ using namespace std;
 void menu(int &pilmenu){
     cout << "===== MENU =====" << endl;
     cout << "================" << endl;
+    cout << "1. Assigenment & Schedule Input" << endl;
+    cout << "2. Leasson Plan" << endl;
+    cout << "3. Check-List Leasson " << endl;
+    cout << "4. Notes" << endl;
+    cout << " Select the Menu : " << endl; 
+    cin >> pilmenu;
+    system ("cls"); 
 }
 
 int main()
 {
+    int pilmenu;
+    
+    do
+    {
+        menu(pilmenu);
+    } while (pilmenu != 4);
     
 }
