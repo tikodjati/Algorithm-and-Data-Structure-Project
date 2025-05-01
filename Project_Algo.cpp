72ad7a81cd2614442c3a2b72095f64700b909b22
tikodjati08@gmail.com <tikodjati08@gmail.com>
2025-05-01 18:28:06 +0700

    Struct Making

--- Project_Algo.cpp
+++ Project_Algo.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
+struct input_schedule{
+    string nama_mks, hari_mks;
+    int jmlh_sks_mks, tingkat_kesulitan_mks;
+};
+
+struct input_assigment{
+    string nama_mka, hari_mka;
+    int jmlh_sks_mka, tingkat_kesulitan_mka;
+};
+
+input_schedule schedule;
+input_assigment assigment;
+
 void menu(int &pilmenu){
     cout << "============= MENU =============" << endl;
     cout << "================================" << endl;
