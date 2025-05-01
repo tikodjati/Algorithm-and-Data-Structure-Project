d4ce4c56351816b129587a4640e6831a64c7b644
oktfiaa <okta77131@gmail.com>
2025-05-01 17:25:23 +0700

    tambah 2 line menu

--- Project Algo.cpp
+++ Project Algo.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 using namespace std;
 
+void menu(int &pilmenu){
+    cout << "===== MENU =====" << endl;
+    cout << "================" << endl;
+}
+
 int main()
 {
-    cout << "Hai tiko"; 
+    
 }
