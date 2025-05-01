48d96ef2ba2fac61179fca51f80f9b17811b6261
oktfiaa <okta77131@gmail.com>
2025-05-01 18:12:25 +0700

    change menu

--- Project_Algo.cpp
+++ Project_Algo.cpp
@@ -2,14 +2,16 @@
 using namespace std;
 
 void menu(int &pilmenu){
-    cout << "===== MENU =====" << endl;
-    cout << "================" << endl;
+    cout << "============= MENU =============" << endl;
+    cout << "================================" << endl;
     cout << "1. Assigenment & Schedule Input" << endl;
     cout << "2. Leasson Plan" << endl;
     cout << "3. Check-List Leasson " << endl;
     cout << "4. Notes" << endl;
-    cout << " Select the Menu : " << endl; 
-    cin >> pilmenu;
+    cout << "================================" << endl;
+    cout << " Select the Menu : " << endl;
+    cin >> pilmenu; 
+    cout << "================================" << endl;
     system ("cls"); 
 }
 
