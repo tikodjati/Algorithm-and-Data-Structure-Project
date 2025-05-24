7baa6e80a1b36444733abe29d07392554f3b8ce4
oktfiaa <okta77131@gmail.com>
2025-05-24 17:38:00 +0700

    update struct linked list

--- Project_Algo.cpp
+++ Project_Algo.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
 using namespace std;
 
-struct input_schedule{
-    string nama_mks, hari_mks;
+//node linked list schedule
+struct node_schedule{
+    char nama_mks[50], hari_mks[50];
     int jmlh_sks_mks, tingkat_kesulitan_mks;
+    node_schedule *next; //pointer ke node berikutnya
 };
 
-struct input_assigment{
-    string nama_mka, hari_mka;
+//node linked list assignment
+struct node_assignment{
+    char nama_mka[50], hari_mka[50],status[20];
     int jmlh_sks_mka, tingkat_kesulitan_mka;
+    node_assignment *next; //pointer ke node assignment
 };
 
-input_schedule schedule;
-input_assigment assigment;
+
+
+// input_schedule schedule;
+// input_assigment assigment;
 
 void menu(int &pilmenu){
     cout << "============= MENU =============" << endl;
