bdf303cca5f3fcb3cdcccb74d3c5fb2415f7dc91
oktfiaa <okta77131@gmail.com>
2025-05-24 17:59:28 +0700

    buat fungsi pembuatan linked list schedule, tambah akhir schedule, dan clear linked list schedule

--- Project_Algo.cpp
+++ Project_Algo.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 //node linked list schedule
@@ -37,7 +38,50 @@ void menu(int &pilmenu)
     cout << " Select the Menu : ";
     cin >> pilmenu;
     cout << "================================" << endl;
-    system("clear");
+    system("cls");
+}
+
+void scheduleLL(string nama_mks, string hari_mks, int jmlh_sks_mks, int tingkat_kesulitan_mks)
+{
+    node_schedule *baru_s = new node_schedule; // buat node baru
+    strncpy(baru_s->nama_mks, nama_mks.c_str(), sizeof(baru_s->nama_mks));
+    strncpy(baru_s->hari_mks, hari_mks.c_str(), sizeof(baru_s->hari_mks));
+    baru_s->jmlh_sks_mks = jmlh_sks_mks;
+    baru_s->tingkat_kesulitan_mks = tingkat_kesulitan_mks;
+
+    baru_s->next = NULL;
+    kepala_s = ekor_s = baru_s;
+}
+
+void TambahAkhirSchedule(string nama_mks, string hari_mks, int jmlh_sks_mks, int tingkat_kesulitan_mks)
+{
+    node_schedule *baru_s = new node_schedule();
+    strncpy(baru_s->nama_mks, nama_mks.c_str(), sizeof(baru_s->nama_mks));
+    strncpy(baru_s->hari_mks, hari_mks.c_str(), sizeof(baru_s->hari_mks));
+    baru_s->jmlh_sks_mks = jmlh_sks_mks;
+    baru_s->tingkat_kesulitan_mks = tingkat_kesulitan_mks;
+    baru_s->next = NULL;
+    if (kepala_s == NULL)
+    {
+        kepala_s = ekor_s = baru_s;
+    }
+    else
+    {
+        ekor_s->next = baru_s;
+        ekor_s = baru_s;
+    }
+}
+
+void clearLL_Schedule()
+{
+    node_schedule *current = kepala_s;
+    while (current != NULL)
+    {
+        node_schedule *hapus = current;
+        current = current->next;
+        delete hapus;
+    }
+    kepala_s = NULL;
 }
 
 int main()
